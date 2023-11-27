3ebd93a2abb87afa0b3b40f24b3b1fd1d538f7c4
Ascanio Vandooren <ascanio@happy-experts.eu>
2023-11-27 08:37:57 +0100

    created Permission Class

--- Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/Lead_ListsDTO.cs
+++ Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/Lead_ListsDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Graph.Models;
+using Microsoft.SharePoint.Client.Sharing;
 
 namespace Ascanio.M365Provisioning.SharePoint.SiteInformation
 {
@@ -14,6 +15,20 @@ namespace Ascanio.M365Provisioning.SharePoint.SiteInformation
         public string EnterpriseKeywords { get; set; } = string.Empty;
         public bool BreakRoleInheritance { get; set; }
         public List<Permission> Permissions { get; set; } = new List<Permission>();
+        public Lead_ListsDTO(string name, string url, string listType, string contentType,
+                             bool showOnQuickLaunch,bool allowFolderCreation, string enterpriseKeyWords,
+                             bool breakRoleInheritiance) 
+        {
+            Name = name;
+            Url = url;
+            ListType = listType;
+            ContentType = contentType;
+            ShowOnQuickLaunch = showOnQuickLaunch;
+            AllowFolderCreation = allowFolderCreation;
+            EnterpriseKeywords = enterpriseKeyWords;
+            BreakRoleInheritance = breakRoleInheritiance;
+
+        }
     }
     
     public class Permission
@@ -29,7 +44,21 @@ namespace Ascanio.M365Provisioning.SharePoint.SiteInformation
         public bool ReadAll { get; set; }
         public bool Write { get; set; }
         public bool WriteAll { get; set; }
-
+        public Permission(string role, bool fullControl, bool edit, 
+                          bool editAll, bool contribute,bool delete, bool deleteAll,
+                          bool read, bool readAll, bool write, bool writeAll)
+        {
+            Role = role;
+            FullControl = fullControl;
+            Edit = edit;
+            EditAll = editAll;
+            Contribute = contribute;
+            Delete = delete;
+            DeleteAll = deleteAll;
+            Read = read;
+            ReadAll = readAll;
+            Write = write;
+            WriteAll = writeAll;
+        }
     }
-
 }
