049668f8ffda8bcdf96a5a79d4f5c4a2a08e77da
Ascanio Vandooren <ascanio@happy-experts.eu>
2023-12-06 07:16:54 +0100

    Test with REST API

--- Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/Lead_WebPartPages.cs
+++ Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/Lead_WebPartPages.cs
@@ -1,6 +1,7 @@
 ﻿using Ascanio.M365Provisioning.SharePoint.Services;
 using Microsoft.SharePoint.Client;
 using Microsoft.SharePoint.Client.WebParts;
+using System.Net.Http.Headers;
 using File = Microsoft.SharePoint.Client.File;
 using List = Microsoft.SharePoint.Client.List;
 
@@ -10,7 +11,7 @@ namespace Ascanio.M365Provisioning.SharePoint.SiteInformation
     {
         public Lead_WebPartPages()
         {
-            using (ClientContext context = new SharePointService().GetClientContext())
+            using  (ClientContext context = new SharePointService().GetClientContext())
             {
                 Console.WriteLine("Lead_SiteSettings.json File created...");
                 IEnumerable<List> Libraries = context.LoadQuery
@@ -35,26 +36,66 @@ namespace Ascanio.M365Provisioning.SharePoint.SiteInformation
                     {
                         File pageFile = sitePage.File;
                         context.Load(pageFile);
+
+                        context.Load(lib.RootFolder);
                         context.ExecuteQuery();
+
+                        Console.WriteLine($"Processing page: {pageFile.ServerRelativeUrl}");
 
                         List pageList = pageFile.ListItemAllFields.ParentList;
-                        LimitedWebPartManager webPartManager = pageFile.GetLimitedWebPartManager(PersonalizationScope.Shared);
-                        context.Load
-                            (
-                            webPartManager.WebParts,
-                            wp => wp.Include(wp => wp.WebPart.Title)
-                            );
-                        context.ExecuteQuery();
-
-                        foreach(WebPartDefinition webPartDefinition in webPartManager.WebParts)
+                        string siteUrl = "https://23m2yz.sharepoint.com/sites/TestSite1";
+                        string endpointUrl = $"{siteUrl}/_api/web/GetFileByServerRelativeUrl('{pageFile.ServerRelativeUrl}')/GetLimitedWebPartManager";
+                        Uri endpointUri = new (endpointUrl, UriKind.Absolute);
+                        _=GetWebParts(context, endpointUri);
+                        string accessToken = context.GetAccessToken();
+                        using (HttpClient client = new HttpClient())
                         {
-                            Console.WriteLine($"WebPart Title : {webPartDefinition.WebPart.Title}");
+                            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                            HttpResponseMessage response = await client.GetAsync(endpointUri);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string result = await response.Content.ReadAsStringAsync();
+                                // Analyseer de resultaten om informatie over de webonderdelen te verkrijgen
+                                //...
+                            }
                         }
+                        //LimitedWebPartManager webPartManager = pageFile.GetLimitedWebPartManager(PersonalizationScope.Shared);
+                        //context.Load
+                        //    (
+                        //    webPartManager.WebParts,
+                        //    wp => wp.Include(wp => wp.WebPart.Title)
+                        //    );
+                        //context.ExecuteQuery();
+                        //Console.WriteLine($"Number of web parts on the page: {webPartManager.WebParts.Count}");
+                        //foreach (WebPartDefinition webPartDefinition in webPartManager.WebParts)
+                        //{
+                        //    Console.WriteLine($"WebPart Title : {webPartDefinition.WebPart.Title}");
+                        //}
                     }
                 }
             }
 
             Console.WriteLine("The SharePoint connection is closed");
+        }
+
+        private async Task GetWebParts(ClientContext context, Uri endpointUri)
+        {
+            string accessToken = context.GetAccessToken();
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                HttpResponseMessage response = await client.GetAsync(endpointUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    // Analyseer de resultaten om informatie over de webonderdelen te verkrijgen
+                    //...
+                }
+            }
         }
     }
 }
