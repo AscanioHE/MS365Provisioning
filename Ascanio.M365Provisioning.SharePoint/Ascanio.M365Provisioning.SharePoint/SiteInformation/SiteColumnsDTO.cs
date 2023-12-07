54475c1a23ecc7a2e41ece8e600810139540ebda
Ascanio Vandooren <ascanio@happy-experts.eu>
2023-12-07 04:50:24 +0100

    Creation of SiteColumnsDTO

--- Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/SiteColumnsDTO.cs
+++ Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/SiteColumnsDTO.cs
@@ -2,5 +2,16 @@
 {
     public class SiteColumnsDTO
     {
+        public string Name {  get; set; }  = string.Empty;
+        public string SchemaXML { get; set; } = string.Empty;  
+        public string DefaultValue {  get; set; } = string.Empty;
+
+        public SiteColumnsDTO(string name, string schemaXML,string defaultValue) 
+        { 
+            Name = name;
+            SchemaXML = schemaXML;
+            DefaultValue = defaultValue;
+        }
+
     }
 }
