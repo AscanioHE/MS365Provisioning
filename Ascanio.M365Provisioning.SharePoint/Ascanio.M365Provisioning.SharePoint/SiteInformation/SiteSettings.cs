79e23f5d87e62c9b8f5df4a4830a51d273d373b0
Ascanio Vandooren <ascanio@happy-experts.eu>
2023-12-08 14:19:03 +0100

    removed comments

--- Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/SiteSettings.cs
+++ Ascanio.M365Provisioning.SharePoint/Ascanio.M365Provisioning.SharePoint/SiteInformation/SiteSettings.cs
@@ -13,47 +13,6 @@ namespace Ascanio.M365Provisioning.SharePoint.SiteInformation
     {
         public SiteSettings()
         {
-            //SharePointService sharePointService = new();
-            //ClientContext context = sharePointService.GetClientContext();
-            //Web web = context.Web;
-            //context.Load(
-            //                web
-            //            );
-            //context.ExecuteQuery();
-
-            //WebTemplateCollection webtTemplateCollection = web.GetAvailableWebTemplates(1033, true);
-            //context.Load(webtTemplateCollection);
-            //context.ExecuteQuery();
-
-            //List<SiteSettingsDTO> webTemplatesDTO = new();
-
-            //foreach (WebTemplate template in webtTemplateCollection)
-            //{
-            //    if (!template.IsHidden)
-            //    {
-            //        // Create a Lead_SiteSettingsDTO and add it to the list
-            //        webTemplatesDTO.Add(new SiteSettingsDTO
-            //        {
-            //            SiteTemplate = template.Name,
-            //            Value = template.Lcid
-            //            // Other properties as needed
-            //        });
-            //    }
-            //}
-            //try
-            //{
-            //    // Execute the query to retrieve the data
-            //    context.ExecuteQuery();
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"Error executing query: {ex.Message}");
-            //    // Handle the exception as needed
-            //}
-            //string jsonFilePath = sharePointService.SiteSettingsFilePath;
-            //WriteData2Json writeData2Json = new();
-            //writeData2Json.Write2JsonFile(webTemplatesDTO,jsonFilePath);
-            //context.Dispose();
         }
 
         public List<SiteSettingsDTO> Load()
