04dc4aa94abbd3e9fbb1bd485a152ce7cf491bef
Ascanio Vandooren <ascanio@happy-experts.eu>
2023-12-19 10:18:23 +0100

    New service

---
+++ MS365Provisioning.SharePoint/MS365Provisioning.SharePoint/Services/ISharePointService.cs
@@ -0,0 +1,9 @@
+﻿using MS365Provisioning.SharePoint.Model;
+
+namespace MS365Provisioning.SharePoint.Services
+{
+    public interface ISharePointService
+    {
+        List<ListsSettingsDto> GetListsSettings();
+    }
+}
---
+++ MS365Provisioning.SharePoint/MS365Provisioning.SharePoint/Services/SharePointService.cs
@@ -0,0 +1,12 @@
+﻿using MS365Provisioning.SharePoint.Model;
+
+namespace MS365Provisioning.SharePoint.Services
+{
+    internal class SharePointService : ISharePointService
+    {
+        public List<ListsSettingsDto> GetListsSettings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
